4fd1c48b972704b39853de97d9975db85e6101cf
selvendranks <selvendranks@gmail.com>
2022-09-27 18:50:09 +0530

    added selectors to JQuery folder

---
+++ 17JQuery/1Selectors/1Selector.js
@@ -0,0 +1,28 @@
+function func1(){
+    
+    //$(element name).action()
+    //document.querySelector(element name)
+
+    // $("div").css('background-color','orange')
+    
+    // $("#div1").css('background-color','orange')  //select by id
+
+    // $("#p1").css('font-style','italic')
+
+    // $(".p1").css('font-style','italic') //select by class
+
+    // $("#div1,#div3").css('background-color','orange') //multiple id
+
+    // $("#div1, li").css('background-color','purple') 
+
+    // $("div  p").fadeToggle(); //access para inside div
+
+    // $("div  p:first").fadeToggle(); //access 1st para inside div
+
+    // $("li:even").fadeToggle(); //access even elements
+
+    // $("li:odd").fadeToggle(); //access odd elements
+
+    // $("p[class!=p1]").fadeToggle()  //access para where class not equals p1
+
+}
